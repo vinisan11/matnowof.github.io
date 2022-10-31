a39964b403827762b4880d24824c3029a4a3679b
matnowof <90075447+matnowof@users.noreply.github.com>
2022-10-31 20:43:07 -0300

    Update sw.js

--- AppMN/sw.js
+++ AppMN/sw.js
@@ -1,3 +1,4 @@
+
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
@@ -5,7 +6,38 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+const resourcesTo Precache = {
+'/',
+'./indexApp.html',
+'/javascript_nova/gzip1041-2.js',
+'./gzipea95.js',
+'./gzip1041.js',
+'./gzipe0f1.js',
+'./gzip8049.js',
+'./gzip8252.js',
+'./gzipdd81.js',
+'./basica-a.min.js',
+'./basica-c.min.js',
+'./basica-e.min.js',
+'/javascript_nova/bsc-a.min2.js',
+'/css/uikit.min.css',
+'/imagens/logo2',
+'/imagens/branco/Prancheta1copia2.png',
+'/imagens/branco/Prancheta1copia8.png',
+'/imagens/branco/Prancheta1copia5.png',
+'/imagens/branco/Prancheta1copia2.png',
+'/imagens/branco/Prancheta1copia10.png',
+'/imagens/branco/Prancheta1copia4.png',
+'/imagens/branco/Prancheta1copia14.png',
+'/imagens/branco/Prancheta1copia13.png',
+'/imagens/branco/Prancheta1copia.png',
+'/imagens/branco/Prancheta1copia12.png',
+'/imagens/branco/Prancheta1copia9.png',
+'/imagens/branco/Prancheta1copia7.png',	
+'/imagens/branco/Prancheta1copia3.png',					
+};
+const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -20,28 +52,9 @@ self.addEventListener('install', async (event) => {
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
+  event.respondWith (
+    caches.match(event.request)  
+       .then(cacheResponse =>(cacheResponse || fetch(event.request)))
+   );
 });
